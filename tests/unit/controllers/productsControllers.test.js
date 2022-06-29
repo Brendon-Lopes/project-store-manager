a9ef704fa60faba4cee51c9f225c3294515d3bcf
Brendon Lopes <brendon.lopes.21@gmail.com>
2022-06-29 18:57:51 -0300

    :white_check_mark: test(productController): adds tests
    Adds tests for the getAll function in the product controllers layer.

---
+++ tests/unit/controllers/productsControllers.test.js
@@ -0,0 +1,64 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const productsController = require('../../../controllers/productsControllers');
+const productsService = require('../../../services/productsServices');
+
+describe('Controller Layer - getAll products route', () => {
+  describe('when it returns correctly', () => {
+    const response = {};
+    const request = {};
+
+    const mockedData = [{
+      'id': 1,
+      'name': "Martelo de Thor",
+    }];
+
+    before(() => {
+      response.status = sinon.stub().returns(response);
+      request.json = sinon.stub().returns();
+
+      sinon.stub(productsService, 'getAll').resolves(mockedData);
+    });
+
+    after(() => {
+      productsService.getAll.restore();
+    });
+
+    it('it\'s called with the status code 200', async () => {
+      await productsController.getAll(request, response);
+      expect(response.status.calledWith(200)).to.be.true;
+    });
+
+    it('it\'s called with the correct data', async () => {
+      await productsController.getAll(request, response);
+      expect(response.json.calledWith(mockedData)).to.be.true;
+    });
+  });
+
+  describe('when the request fails', () => {
+    const response = {};
+    const request = {};
+
+    before(() => {
+      response.status = sinon.stub().returns(response);
+      request.json = sinon.stub().returns();
+
+      sinon.stub(productsService, 'getAll').resolves(false);
+    });
+
+    after(() => {
+      productsService.getAll.restore();
+    });
+
+    it('it\'s called with the status code 404', async () => {
+      await productsController.getAll(request, response);
+      expect(response.status.calledWith(404)).to.be.true;
+    });
+
+    it('it shows the error message', async () => {
+      await productsController.getAll(request, response);
+      expect(response.json.calledWith({ message: 'Products not found' })).to.be.true;
+    });
+  });
+});
