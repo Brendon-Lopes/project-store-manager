08156d5b722d59ed68ee7a358d7d1c97093f68da
Brendon Lopes <brendon.lopes.21@gmail.com>
2022-06-29 17:17:17 -0300

    :sparkles: feat: adds error middleware
    adds an error middleware to catch errors alogside express-rescue.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const app = require('./app');
 require('dotenv').config();
 
-// não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
+const errorMiddleware = require('./middlewares/errorMiddleware');
+
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
---
+++ middlewares/errorMiddleware.js
@@ -0,0 +1,11 @@
+const httpStatusCode = require('../helpers/httpStatusCode');
+
+const errorMiddleware = (err, req, res, _next) => {
+  if (err.code && err.status) {
+    return res.status(err.status).json({ message: err.message, code: err.code });
+  }
+
+  return res.status(httpStatusCode.INTERNAL_SERVER).json({ message: err.message });
+};
+
+module.exports = errorMiddleware;
