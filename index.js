a34f5880150cdb77a835fadfe7a95e0959876c76
Brendon Lopes <brendon.lopes.21@gmail.com>
2022-06-29 17:22:15 -0300

    :sparkles: adds basic route for products

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const { default: rescue } = require('express-rescue');
 const app = require('./app');
 require('dotenv').config();
 
+const router = require('./routers');
+
 const errorMiddleware = require('./middlewares/errorMiddleware');
+
+app.use('/products', rescue(router.productsRouter));
 
 app.use(errorMiddleware);
 
