d4bbdfd14ae619957eae5cc9eb5229f7bbe21631
Brendon Lopes <brendon.lopes.21@gmail.com>
2022-06-30 18:17:27 -0300

    :sparkles: feat: adds a route to create products
    adds a post route on '/products' to create new products.

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -2,9 +2,18 @@ const express = require('express');
 
 const productsRouter = express.Router();
 
+const middleware = require('../middlewares');
 const controller = require('../controllers/productsControllers');
 
 productsRouter.get('/', controller.getAll);
+
+productsRouter.post(
+  '/',
+  middleware.productNameValidation,
+  middleware.productNameLengthValidation,
+  controller.create,
+);
+
 productsRouter.get('/:id', controller.getById);
 
 module.exports = productsRouter;
