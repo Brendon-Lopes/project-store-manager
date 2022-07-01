fab7eae22ba080469fd00775ff55d116d813ebb6
Brendon Lopes <brendon.lopes.21@gmail.com>
2022-07-01 20:23:30 -0300

    :white_check_mark: test: adds tests for controller layer
    adds tests to controller layer - deleteById function on route
    /products/:id

--- tests/unit/controllers/productsControllers.test.js
+++ tests/unit/controllers/productsControllers.test.js
@@ -319,3 +319,83 @@ describe('Controller Layer - update product', () => {
     });
   });
 });
+
+describe('Controller Layer - delete product', () => {
+  describe('when the item is deleted succesfully', () => {
+    const response = {};
+    const request = {};
+
+    before(() => {
+      request.params = { id: 1 };
+
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(productsService, 'deleteById').resolves(true);
+    });
+
+    after(() => {
+      productsService.deleteById.restore();
+    });
+
+    it('is called with the code 204', async () => {
+      await productsController.deleteById(request, response);
+      expect(response.status.calledWith(httpStatusCode.NO_CONTENT)).to.be.true;
+    });
+  });
+
+  describe('when the product does not exist', () => {
+    const response = {};
+    const request = {};
+
+    before(() => {
+      request.params = { id: 1 };
+
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(productsService, 'deleteById').resolves(false);
+    });
+
+    after(() => {
+      productsService.deleteById.restore();
+    });
+
+    it('it\'s called with the status code 404', async () => {
+      await productsController.deleteById(request, response);
+      expect(response.status.calledWith(httpStatusCode.NOT_FOUND)).to.be.true;
+    });
+
+    it('it shows the error message', async () => {
+      await productsController.deleteById(request, response);
+      expect(response.json.calledWith({ message: 'Product not found' })).to.be.true;
+    });
+  });
+
+  describe('when the function throws an error', () => {
+    const response = {};
+    const request = {};
+
+    before(() => {
+      request.params = { id: 1 };
+
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(productsService, 'deleteById').throws(new Error('Internal server error'));
+    });
+
+    after(() => {
+      productsService.deleteById.restore();
+    });
+
+    it('it\'s called with the status code 500', async () => {
+      try {
+        const product = await productsController.deleteById(request, response);
+        expect(product).to.throw(new Error('Internal server error'));
+      } catch (err) {
+        expect(response.status.calledWith(httpStatusCode.INTERNAL_SERVER)).to.be.true;
+      }
+    });
+  });
+});
