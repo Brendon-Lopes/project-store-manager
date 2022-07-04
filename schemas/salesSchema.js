dee2d008dbb1f4041e8b018a95b98735d45071c1
Brendon Lopes <brendon.lopes.21@gmail.com>
2022-07-04 19:02:53 -0300

    :sparkles: refactor: joi validators
    refactor joi validators to be more efficient and simple.

--- schemas/salesSchema.js
+++ schemas/salesSchema.js
@@ -1,21 +1,26 @@
 const Joi = require('joi');
 
-const validateRequired = Joi.object({
-  productId: Joi.number().required()
-    .messages({
+const validateQuantity = Joi.array().required().items(
+  Joi.object({
+    productId: Joi.number(),
+    quantity: Joi.number().required().min(1).messages({
+      'number.min': '"quantity" must be greater than or equal to 1',
+    }),
+  }),
+);
+
+const validateProducts = Joi.array().required().items(
+  Joi.object({
+    productId: Joi.number().required().messages({
       'any.required': '"productId" is required',
     }),
-  quantity: Joi.number().required()
-    .messages({
+    quantity: Joi.number().required().messages({
       'any.required': '"quantity" is required',
     }),
-});
-
-const validateQuantity = Joi.number().min(1).messages({
-  'number.min': '"quantity" must be greater than or equal to 1',
-});
+  }),
+);
 
 module.exports = {
-  validateRequired,
   validateQuantity,
+  validateProducts,
 };
